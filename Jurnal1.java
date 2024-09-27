078706202b82980df2ab635a3f30898e0d8b88c5
Muhammad Rayhan Al Ghifari <oner76136@gmail.com>
2024-09-27 12:59:36 +0700

    firstt

---
+++ Jurnal1.java
@@ -0,0 +1,8 @@
+public class Jurnal1 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Muhammad Rayhan Al-Ghifari");
+        System.out.println("NIM : 6070624700");
+        System.out.println("Kelas : D3 IF-4803");
+    }
+    
+}
